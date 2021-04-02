f7eb67b9d85f1dea1bac99970145dc6ea03b8ae4
Wyckliffe Aluga <>
2021-04-02 18:25:25 +0300

    first django project

---
+++ portfolio/wyckliffe_portfolio/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path , include
+
+urlpatterns = [
+    path('', include('base.html')),
+    path('admin/', admin.site.urls),
+    path('projects/', include('projects.urls')),
+    path('blog/', include("blog.urls")),
+]
