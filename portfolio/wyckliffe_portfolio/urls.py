2868a764b739d9908eee84486241569f549c2c90
Wyckliffe Aluga <>
2021-04-03 17:20:29 +0300

    Update urls.py

--- portfolio/wyckliffe_portfolio/urls.py
+++ portfolio/wyckliffe_portfolio/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('', include('base.html')),
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('blog/', include("blog.urls")),
 ]
+
