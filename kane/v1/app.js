6238135d3c98474f3edc6fcbb6bd874439d289da
Wyckliffe Aluga <>
2021-04-05 01:12:40 +0300

    Update app.js

--- kane/v1/app.js
+++ kane/v1/app.js
@@ -1,15 +1,6 @@
 
 const http = require('http');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit()
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> Hello </title></head>')
-    res.write('<body><h1>Hello from v1 server!</h1></body>')
-    res.write('</html>');
-    res.end();
-});
-
+const server = http.createServer(routes); 
 server.listen(3000);
