edbeedbe0032f74763f9e4ee30193cb4fb6e2ea0
Wyckliffe Aluga <>
2021-04-04 20:59:19 +0300

    Create app.js

---
+++ kane/v1/app.js
@@ -0,0 +1,15 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit()
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> Hello </title></head>')
+    res.write('<body><h1>Hello from v1 server!</h1></body>')
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
