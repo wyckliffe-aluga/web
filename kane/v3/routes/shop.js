55e1608769835ff940f81d4cabd03a8604770d70
Wyckliffe Aluga <>
2021-04-05 13:15:31 +0300

    Update shop.js

--- kane/v3/routes/shop.js
+++ kane/v3/routes/shop.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = require('express').Router(); 
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router; 
