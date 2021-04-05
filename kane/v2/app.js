647363eadfda48bf63cea0f7406bf27e48833053
Wyckliffe Aluga <>
2021-04-05 11:56:38 +0300

    v2 updates

--- kane/v2/app.js
+++ kane/v2/app.js
@@ -1,14 +1,19 @@
 const express = require('express'); 
+const bodyParser = require('body-parser');
 
 const app = express() ; 
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/add-product', (req, res, next) => {
-    console.log('another middle'); 
-    res.send('<h1>Add Product!</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" id="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('another middle'); 
     res.send('<h1>Hello from express!</h1>');
 });
 
