977401841813e5e582c28b16bf6714df485cd550
Wyckliffe Aluga <>
2021-04-05 17:32:10 +0300

    Update app.js

--- kane/v3/app.js
+++ kane/v3/app.js
@@ -1,9 +1,13 @@
 const express = require('express'); 
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express() ; 
 const adminRoutes = require('./routes/admin') ; 
 const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
