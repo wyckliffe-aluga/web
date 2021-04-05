71266710e5e6f6ff080d91fe0b882a6c8837ef4e
Wyckliffe Aluga <>
2021-04-05 13:05:09 +0300

    Update admin.js

--- kane/v3/routes/admin.js
+++ kane/v3/routes/admin.js
@@ -1,11 +1,13 @@
 const express = require('express'); 
+const path = require('path');
+
 const router = require('express').Router(); 
 const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({extended: false}))
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" id="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
