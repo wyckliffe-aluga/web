52d5c3110ea264aa1978ee24a95ce0c9f7b1a7d4
Wyckliffe Aluga <>
2021-04-06 18:01:54 +0300

    Update admin.js

--- kane/v3/routes/admin.js
+++ kane/v3/routes/admin.js
@@ -5,12 +5,11 @@ const router = require('express').Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.render('add-product.pug', {pageTitle: 'Add Product'})
 });
 
 router.post('/add-product', (req, res, next) => {
     products.push({title: req.body.title})
-    console.log(products);
     res.redirect('/');
 });
 
