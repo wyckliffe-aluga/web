58b2fa4aa9fc78b09f1f71d003f7331cf4e9139b
Wyckliffe Aluga <>
2021-04-07 15:43:18 +0300

    Update admin.js

--- kane/v3/routes/admin.js
+++ kane/v3/routes/admin.js
@@ -1,10 +1,13 @@
 const express = require('express'); 
 const router = require('express').Router();
 
-const productsController = require('../controllers/products')
+const adminController = require('../controllers/admin')
 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+router.get('/products', adminController.getProducts); 
+
+router.post('/add-product', adminController.postAddProduct);
+
 
 module.exports = router;
