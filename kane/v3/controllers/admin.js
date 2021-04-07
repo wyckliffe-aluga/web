bfcc4ab5fb183d0c2f8cdc13484e11d6cee763fc
Wyckliffe Aluga <>
2021-04-07 18:52:13 +0300

    Update admin.js

--- kane/v3/controllers/admin.js
+++ kane/v3/controllers/admin.js
@@ -2,12 +2,10 @@ const Product = require('../model/product');
 
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
         pageTitle: 'Add Product', 
         path: '/admin/add-product',
-        formsCSS: true, 
-        productCSS: true, 
-        activeAddProduct: true
+        editing: false, 
     });
 }; 
 
@@ -22,6 +20,29 @@ exports.postAddProduct = (req, res, next) => {
     res.redirect('/');
 };
 
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+      return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    });
+  };
+  
+exports.postEditProduct = (req, res, next) => {
+    
+}
+
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('admin/products', {
@@ -31,3 +52,4 @@ exports.getProducts = (req, res, next) => {
         });
     });
 };
+
