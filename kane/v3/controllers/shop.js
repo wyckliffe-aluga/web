786e1f4ed592db24cc8fe33089655464e8fdb068
Wyckliffe Aluga <>
2021-04-07 15:42:48 +0300

    Create shop.js

---
+++ kane/v3/controllers/shop.js
@@ -0,0 +1,42 @@
+const Product = require('../model/product');
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/product-list', {
+            prods: products, 
+            pageTitle: 'All Products', 
+            path: '/product-list', 
+        });
+    });
+};
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/index', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/', 
+        });
+    });
+};
+
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'My Cart',
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+    res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout' 
+    });
+};
+
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'My Orders',
+    });
+};
