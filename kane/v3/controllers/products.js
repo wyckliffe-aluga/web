03adbd900af95361606bb8bed5651236c2f21906
Wyckliffe Aluga <>
2021-04-07 13:14:33 +0300

    Update products.js

--- kane/v3/controllers/products.js
+++ kane/v3/controllers/products.js
@@ -17,14 +17,14 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProduct: products.length > 0, 
-        activeShop: true, 
-        productsCSS: true
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/', 
+            hasProduct: products.length > 0, 
+            activeShop: true, 
+            productsCSS: true
+        });
     });
 };
