b0c6a52c5d82c461b1889ad5e121842d05032467
Wyckliffe Aluga <>
2021-04-07 09:06:18 +0300

    Update app.js

--- kane/v3/app.js
+++ kane/v3/app.js
@@ -1,9 +1,12 @@
 const express = require('express'); 
 const path = require('path');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express() ; 
 
+//app.engine('handlebars', expressHbs());
+app.set('view engine', 'handlebars');
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
@@ -17,7 +20,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: '404'})
+    res.status(404).render('404', {pageTitle: 'Page Not Found'})
 });
 
 app.listen(3000);
