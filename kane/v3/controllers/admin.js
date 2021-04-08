8bc9d87d6438fa4d19316f3baada20518de87076
Wyckliffe Aluga <>
2021-04-09 00:06:02 +0300

    Update admin.js

--- kane/v3/controllers/admin.js
+++ kane/v3/controllers/admin.js
@@ -15,8 +15,12 @@ exports.postAddProduct = (req, res, next) => {
     const description = req.body.description;
 
     const product = new Product(null, title, imageUrl, description, price);
-    product.save();
-    res.redirect('/');
+    product
+      .save()
+      .then(res.redirect('/'))
+      .catch( err => {
+        console.log(err);
+      });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -50,18 +54,26 @@ exports.postEditProduct = (req, res, next) => {
         updatedDesc, 
         updatedPrice); 
         
-    updatedProduct.save(); 
-    res.redirect('/admin/products')
+    updatedProduct
+    .save()
+    .then(res.redirect('/admin/products'))
+    .catch(err => {
+      console.log(err);
+    }); 
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
-            prods: products, 
-            pageTitle: 'Admin Products', 
-            path: '/admin/products', 
-        });
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
+    res.render('admin/products', {
+      prods: rows,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
     });
+  })
+  .catch(err => {
+    console.log(err)
+  });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
