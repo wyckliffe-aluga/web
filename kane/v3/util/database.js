92388a16dabf755824727f5a391d38bff9852d3c
Wyckliffe Aluga <>
2021-04-09 02:21:44 +0300

    Update database.js

--- kane/v3/util/database.js
+++ kane/v3/util/database.js
@@ -1,11 +1,19 @@
 
-const mysql = require('mysql2') ; 
+//const mysql = require('mysql2') ; 
 
-const pool  = mysql.createPool({
-    host: 'localhost', 
-    user: 'root', 
-    database: 'node-complete', 
-    password: 'PjK5vYfR'
+//const pool  = mysql.createPool({
+//    host: 'localhost', 
+//    user: 'root', 
+//    database: 'node-complete', 
+//    password: 'PjK5vYfR'
+//});
+//module.exports = pool.promise(); 
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'PjK5vYfR', {
+    dialect: 'mysql', 
+    host: 'localhost'
 });
 
-module.exports = pool.promise(); 
+module.exports = sequelize ; 
