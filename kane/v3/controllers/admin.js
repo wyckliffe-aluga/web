ebed2da98a29b1a675c6d652bfae175e900598dd
Wyckliffe Aluga <>
2021-04-09 02:21:30 +0300

    Update admin.js

--- kane/v3/controllers/admin.js
+++ kane/v3/controllers/admin.js
@@ -1,23 +1,32 @@
 const Product = require('../model/product');
+const { v1: uuidv1, v4: uuidv4} = require('uuid');
 
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/edit-product', {
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product',
-        editing: false, 
-    });
-}; 
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
+    const id = uuidv4().toString();
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(null, title, imageUrl, description, price);
-    product
-      .save()
-      .then(res.redirect('/'))
+    Product.create({
+      id: id,
+      title: title,
+      imageUrl: imageUrl,
+      price: price, 
+      description: description
+    })
+      .then( result => {
+        console.log('Product Created Successfully')
+        res.redirect('/admin/products');
+      })
       .catch( err => {
         console.log(err);
       });
@@ -29,17 +38,21 @@ exports.getEditProduct = (req, res, next) => {
       return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
+    Product.findOne({where: {id: prodId}})
+      .then(product => {
+        if (!product) {
+          return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+          pageTitle: 'Edit Product', 
+          path: '/admin/edit-product', 
+          editing: editMode, 
+          product: product
+        });
+      })
+      .catch( err => {
+        console.log(err)
       });
-    });
   };
 
 exports.postEditProduct = (req, res, next) => {
@@ -48,25 +61,29 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price ; 
     const updatedImageUrl = req.body.imageUrl; 
     const updatedDesc = req.body.description; 
-    const updatedProduct = new Product(prodId, 
-        updatedTitle, 
-        updatedImageUrl, 
-        updatedDesc, 
-        updatedPrice); 
-        
-    updatedProduct
-    .save()
-    .then(res.redirect('/admin/products'))
-    .catch(err => {
-      console.log(err);
+
+    Product.findOne({where: {id: prodId}})
+      .then(product => {
+        product.title = updatedTitle; 
+        product.price = updatedPrice;
+        product.imageUrl = updatedImageUrl; 
+        product.description = updatedDesc;
+        return product.save();
+      })
+      .then(result => {
+        console.log("Updated Product");
+        res.redirect('/admin/products');
+      })
+      .catch(err => {
+         console.log(err);
     }); 
-}
+};
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
+  Product.findAll()
+  .then(products => {
     res.render('admin/products', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
@@ -78,6 +95,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId; 
-    Product.deleteById(prodId); 
-    res.redirect('/admin/products')
+    Product.findOne({where: {id: prodId}})
+      .then(product => {
+        return product.destroy();
+      })
+      .then( result => {
+        console.log('Product destroyed');
+        res.redirect('/admin/products');
+      })
+      .catch(err => {
+        console.log(err);
+    }); 
 }
