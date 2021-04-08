87f23520837b673178698ebd99e93a78d832b20a
Wyckliffe Aluga <>
2021-04-09 02:21:34 +0300

    Update shop.js

--- kane/v3/controllers/shop.js
+++ kane/v3/controllers/shop.js
@@ -3,10 +3,10 @@ const Cart = require('../model/cart');
 
 exports.getProducts = (req, res, next) => {
 
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products'
       });
@@ -18,10 +18,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findOne({where : {id : prodId}})
+    .then(product => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
@@ -33,16 +33,16 @@ exports.getProduct = (req, res, next) => {
 
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then(products => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
         path: '/'
       });
     })
-    .catch(err => {
-      console.log(err)
+    .catch( err => {
+      console.log(err);
     });
 };
 
