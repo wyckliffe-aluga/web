f17e8d22f136fde2180275a0b6ef0816286e17d8
Wyckliffe Aluga <>
2021-04-09 02:21:40 +0300

    Update product.js

--- kane/v3/model/product.js
+++ kane/v3/model/product.js
@@ -1,33 +1,34 @@
-const { v1: uuidv1, v4: uuidv4, } = require('uuid'); 
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequelize = require('sequelize'); 
+const sequelize = require('../util/database');
 
 
-
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product', {
+  key: {
+    type: Sequelize.INTEGER, 
+    autoIncrement: true, 
+    allowNull: false, 
+    primaryKey: true, 
+  },
+  id: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  title:{
+    type:Sequelize.STRING,
+    allowNull: false
+  }, 
+  price : {
+    type: Sequelize.DOUBLE, 
+    allowNull: false
+  }, 
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }, 
+  description:{
+    type: Sequelize.TEXT('long'), 
+    allowNull: false
   }
+});
 
-  save() {
-    return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)', 
-      [this.title, this.price, this.description, this.imageUrl]
-    );
-  }
-
-  static deleteById(id) {
-    return db.execute('DELETE * FROM products WHERE products.id = ?', [id]);
-  }
-
-  static fetchAll() {
-   return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id, cb) { 
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Product; 
