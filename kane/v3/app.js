5a868b00c01339ba9bcbe6b118064c55c8f0b3bf
Wyckliffe Aluga <>
2021-04-09 00:05:58 +0300

    Update app.js

--- kane/v3/app.js
+++ kane/v3/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express() ; 
 
