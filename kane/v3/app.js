700b61f55c16f0a019fd8fe1b72afd7490a45b80
Wyckliffe Aluga <>
2021-04-09 03:58:29 +0300

    Update app.js

--- kane/v3/app.js
+++ kane/v3/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./model/product');
+const User = require('./model/user');
 
 const app = express() ; 
 
@@ -18,14 +20,46 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findOne({where: {id: 'gqpkkn9j7m4k'}})
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// A user created this product
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
+     //.sync({force: true})
     .sync()
     .then( result => {
+        return User.findOne({where: {id: 'gqpkkn9j7m4k'}});
+    })
+    .then( user => {
+        if(!user) {
+            return  User.create({
+                id: 'gqpkkn9j7m4k', 
+                firstName: 'Tony', 
+                lastName: 'Stark',
+                email: 'tony@stark.com'
+            });
+        return Promise.resolve(user);
+        }
+    })
+    .then(user => {
+        //console.log(user);
         app.listen(3000);
     })
     .catch(err => {
