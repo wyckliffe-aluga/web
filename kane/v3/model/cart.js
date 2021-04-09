489adde77af34752e41b763168ca79dcdfae1379
Wyckliffe Aluga <>
2021-04-09 13:09:21 +0300

    Update cart.js

--- kane/v3/model/cart.js
+++ kane/v3/model/cart.js
@@ -6,7 +6,7 @@ const Cart = sequelize.define('cart', {
       type: Sequelize.INTEGER, 
       autoIncrement: true,
       allowNull: false, 
-      primaryKey:true
+      primaryKey:true,
     }
 }); 
 
