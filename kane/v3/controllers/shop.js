c4e2a55a9c38d40e5e243e2ebfdcab33d745f02f
Wyckliffe Aluga <>
2021-04-09 14:37:30 +0300

    Update shop.js

--- kane/v3/controllers/shop.js
+++ kane/v3/controllers/shop.js
@@ -125,37 +125,46 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart ;
-  req.user 
+  let fetchedCart;
+  req.user
     .getCart()
     .then(cart => {
       fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
-     return req.user.createOrder({orderNo: orderid.getTime(id).toString()})
+      return req.user
+      .createOrder({orderNo : orderid.getTime(id).toString()})
         .then(order => {
-          order.addProducts(products.map(product => {
-            product.orderItem = {quantity: product.cartItem.quantity };
-            return product;
-          }));
+          return order.addProducts(
+            products.map(product => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
         })
         .catch(err => console.log(err));
     })
     .then(result => {
       return fetchedCart.setProducts(null);
     })
-    .then( result => {
+    .then(result => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user
+    .getOrders({ include: ['products']})
+    .then(orders => {
+      res.render('shop/orders', {
+        orders: orders,
+        path: '/orders',
+        pageTitle: 'Your Orders'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
