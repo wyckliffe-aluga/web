412f8dd9640ea57458fb580e25734e935ac0aed7
Wyckliffe Aluga <>
2021-04-09 06:17:22 +0300

    Update user.js

--- kane/v3/model/user.js
+++ kane/v3/model/user.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
 const User = sequelize.define('user', {
-    key: {
+    id: {
         type: Sequelize.INTEGER, 
         autoIncrement: true, 
         allowNull: false, 
         primaryKey: true, 
     },
-    id: {
+    key: {
         type: Sequelize.STRING,
         allowNull: false,
     },
