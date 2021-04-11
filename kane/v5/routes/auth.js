a5196a9d16ca5fb69d69bd010dc59b60aaed5942
Wyckliffe Aluga <>
2021-04-11 23:51:25 +0300

    Update auth.js

--- kane/v5/routes/auth.js
+++ kane/v5/routes/auth.js
@@ -6,5 +6,7 @@ const { route } = require('./admin');
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.post('/logout', authController.postLogout);
+router.get('/signup', authController.getSignUp);
+router.post('/signup', authController.postSignUp);
 
 module.exports = router; 
