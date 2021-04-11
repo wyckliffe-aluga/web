ff27a6e12b72ba024074ffc9e6a97dcc3f9542e0
Wyckliffe Aluga <>
2021-04-11 21:58:36 +0300

    Create auth.js

---
+++ kane/v5/controllers/auth.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+
+
+exports.getLogin = (req, res, next) => {
+    res.render('auth/login', {
+        path: '/login', 
+        pageTitle: 'Login',
+        isAuthenticated: false
+    });
+}
+
+exports.postLogin = (req, res, next) => {
+    User.findById('6070a2dc86973f41d8264237')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                console.log(err)
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+    };
+
+    
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
+};
