157f0f8ad645d9b1a132f36dac3dcc5ae1bc6ca1
Wyckliffe Aluga <>
2021-04-12 01:31:00 +0300

    Update auth.js

--- kane/v5/controllers/auth.js
+++ kane/v5/controllers/auth.js
@@ -2,10 +2,16 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0]
+    } else {
+        message = null;
+    }
     res.render('auth/login', {
         path: '/login', 
         pageTitle: 'Login',
-        isAuthenticated: false
+        errorMessage: message
     });
 }
 
@@ -15,6 +21,7 @@ exports.postLogin = (req, res, next) => {
     User.findOne({ email: email })
       .then(user => {
         if (!user) {
+            req.flash('error', 'Invalid email or password.');
           return res.redirect('/login');
         }
         bcrypt
@@ -28,6 +35,7 @@ exports.postLogin = (req, res, next) => {
                 res.redirect('/');
               });
             }
+            req.flash('error', 'Invalid email or password.');
             res.redirect('/login');
           })
           .catch(err => {
@@ -46,10 +54,16 @@ exports.postLogout = (req, res, next) => {
 };
 
 exports.getSignUp = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0]
+    } else {
+        message = null;
+    }
     res.render('auth/signup' , {
         path: '/signup', 
         pageTitle: 'SignUp',
-        isAuthenticated: false
+        errorMessage: message
     });
 };
 
@@ -63,6 +77,7 @@ exports.postSignUp = (req, res, next) => {
     User.findOne({email: email})
         .then(userDoc => {
             if (userDoc) {
+                req.flash('error', 'e-mail exists already, please pick another email.');
                 return res.redirect('/signup');
             }
             return bcrypt
