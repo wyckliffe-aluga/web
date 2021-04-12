fb5af8ff1c1a3e1257d02577b9d0cedfa518069c
Wyckliffe Aluga <>
2021-04-12 17:58:58 +0300

    Update admin.js

--- kane/v5/routes/admin.js
+++ kane/v5/routes/admin.js
@@ -7,19 +7,15 @@ const isAuth =require('../middleware/is-auth');
 
 const router = express.Router();
 
-// /admin/add-product => GET
+//Add products
 router.get('/add-product', isAuth, adminController.getAddProduct);
-
-// /admin/products => GET
-router.get('/products', isAuth, adminController.getProducts);
-
-// /admin/add-product => POST
 router.post('/add-product', isAuth, adminController.postAddProduct);
-
+//get products
+router.get('/products', isAuth, adminController.getProducts);
+//edit products
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
-
 router.post('/edit-product', isAuth, adminController.postEditProduct);
-
+//delete products
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
